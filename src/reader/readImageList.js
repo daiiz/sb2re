3b5de5b43202519ac67d08fd9c58a1f3d0652b66
daiiz <da.iz.mg@gmail.com>
2019-02-04 22:56:51 +0900

    gyazo raw urls

---
+++ src/reader/readImageList.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const {parse} = require('url')
+
+const imageList = './out/images/urls.txt'
+
+const getGyazoId = url => {
+  const { host, pathname} = parse(url)
+  if (!/gyazo\.com$/.test(host)) return null
+  return pathname.split('/')[1]
+}
+
+const getImageUrls = () => {
+  const lines = fs.readFileSync(imageList, 'utf-8').split('\n')
+  const gyazoIds = lines.map(line => getGyazoId(line)).filter(id => !!id)
+  return gyazoIds.map(id => `https://gyazo.com/${id}/raw`)
+}
+
+module.exports = {getImageUrls}
