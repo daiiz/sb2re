7a580dd7b821b7067d5739023d7000a3aaa5cda0
daiiz <da.iz.mg@gmail.com>
2019-02-06 03:33:48 +0900

    0.5で丁度いい

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -65,7 +65,7 @@ const renderLine = (lastIndentSize, line, opts) => {
       case 'gyazo': {
         const url = tok.text
         text = [
-          `//image[${getGyazoId(url)}][][scale=0.65]{`, // [fileName][Caption]
+          `//image[${getGyazoId(url)}][][scale=0.5]{`, // [fileName][Caption]
           '//}'
         ].join('\n')
         break
@@ -73,7 +73,7 @@ const renderLine = (lastIndentSize, line, opts) => {
       case 'gyazoWithLabel': {
         const url = tok.text.url
         text = [
-          `//image[${getGyazoId(url)}][][scale=0.65]{`, // [fileName][Caption]
+          `//image[${getGyazoId(url)}][][scale=0.5]{`, // [fileName][Caption]
           '//}'
         ].join('\n')
         break
