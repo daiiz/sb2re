17cd3d5a2ce22396fe87d4a1f83836b8081054ab
daiiz <da.iz.mg@gmail.com>
2019-02-10 03:17:18 +0900

    jpg画像はpngに変換して使う

--- src/downloader.js
+++ src/downloader.js
@@ -1,8 +1,25 @@
 const request = require('request')
+const jimp = require('jimp')
 const fs = require('fs')
 const imagesDir = './out/images'
 
-const skipIfExists = false
+const skipIfExists = true
+
+const getExt = contentType => {
+  switch (contentType) {
+    case 'image/png':
+      return 'png'
+    case 'image/jpeg':
+    case 'image/jpg':
+      return 'jpg'
+    default:
+      return null
+  }
+}
+
+const existsImage = (prefix, gyazoId) => {
+  return fs.existsSync(`${imagesDir}/${prefix}${gyazoId}.png`)
+}
 
 const downloadImages = (gyazoIds, callback, {size, prefix}) => {
   prefix = prefix || ''
@@ -11,22 +28,34 @@ const downloadImages = (gyazoIds, callback, {size, prefix}) => {
   } else {
     const gyazoId = gyazoIds.pop()
     // すでにfetch済みであればskip
-    if (skipIfExists &&
-      (fs.existsSync(`${imagesDir}/${prefix}${gyazoId}.png`) ||
-      fs.existsSync(`${imagesDir}/${prefix}${gyazoId}.jpeg`))) {
-      console.log(`> skip (${gyazoId})`)
+    if (!gyazoId || (skipIfExists && existsImage(prefix, gyazoId))) {
+      if (gyazoId) console.log(`> skip (${gyazoId})`)
       downloadImages(gyazoIds, callback, {size, prefix})
     } else {
       const url = `https://gyazo.com/${gyazoId}/thumb/${size}`
-      request.get(url)
-        .on('response', res => {
-          // const contentType = res.headers['content-type']
-        })
-        .pipe(fs.createWriteStream(`${imagesDir}/${prefix}${gyazoId}.png`))
-        .on('close', () => {
-          console.log(`> (${gyazoIds.length}) ${imagesDir}/${prefix}${gyazoId}`)
+      request.get(url, {
+        method: 'GET',
+        encoding: null
+      }, (err, res, body) => {
+          const contentType = res.headers['content-type']
+          const ext = getExt(contentType)
+          if (!ext) {
+            console.log('> Not supported format:', contentType, url)
+          } else {
+            const imagePath = `${imagesDir}/${prefix}${gyazoId}.${ext}`
+            fs.writeFileSync(imagePath, body, 'binary')
+            if (ext === 'jpg') {
+              // jpgを扱うとなぜかreview-pdfmakerがエラーを出す。詳細不明。
+              // 仕方ないのでpngに変換する。
+              jimp.read(imagePath, (err, img) => {
+                img.write(`${imagesDir}/${prefix}${gyazoId}.png`)
+                fs.unlinkSync(imagePath)
+              })
+            }
+            console.log(`> (${gyazoIds.length}) ${imagesDir}/${prefix}${gyazoId}`)
+          }
           downloadImages(gyazoIds, callback, {size, prefix})
-        })
+      })
     }
   }
 }
